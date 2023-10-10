bec7e7b4f87410aeb678f270fae13608f53c69a2
Mabe0116 <115063548+Mabe0116@users.noreply.github.com>
2023-10-10 23:43:33 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+template <typename Type>
+
+Type Min(Type a, Type b) {
+	if (a < b) {
+		return b;
+	}
+	return a;
+}
+
+template <>
+char Min <char>(char a, char b)
+{
+	printf("”šˆÈŠO‚Í‘ã“ü‚Å‚«‚Ü‚¹‚ñ");
+	return 0;
+}
+
+
+int main() {
+
+	const int kWindowWigth = 1280;
+
+	printf("%d\n", Min<int>(114, 514));
+	printf("%f\n", Min<float>(11.4f, 51.4f));
+	printf("%lf\n", Min<double>(11.4f, 51.4f));
+	printf("%c\n", Min<char>('v', 'x'));
+
+	return 0;
+}
+
+
