0d19785462385a922b0e7aa79b36e7e7d569e9b4
k022g0069@g.neec.ac.jp <k022g0069@g.neec.ac.jp>
2023-12-12 09:44:42 +0900

    課題終了

--- main.cpp
+++ main.cpp
@@ -1,32 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <Windows.h>
 
-template <typename Type>
+typedef void (pFunc)(int a);
 
-Type Min(Type a, Type b) {
-	if (a < b) {
-		return b;
-	}
-	return a;
+void DispResult(int answer) {
+    int result = rand() % 6 + 1;
+    if (result % 2 == 0) {
+        if (answer == 1) {
+            printf("サイコロの目:%d : 不正解\n", result);
+        }
+        else if (answer == 2) {
+            printf("サイコロの目:%d : 正解\n", result);
+        }
+    }
+    else if (result % 2 == 1) {
+        if (answer == 1) {
+            printf("サイコロの目:%d : 正解\n", result);
+        }
+        else if (answer == 2) {
+            printf("サイコロの目:%d : 不正解\n", result);
+        }
+    }
 }
 
-template <>
-char Min <char>(char a, char b)
-{
-	printf("数字以外は代入できません");
-	return 0;
+void SetTimeout(pFunc p, int answer) {
+    Sleep(3 * 1000);
+    p(answer);
 }
 
+void Game(pFunc p, int second) {
+    printf("奇数か偶数を選んでください(1:奇数, 2:偶数)\n");
+    int answer;
+    scanf_s("%d", &answer);
+    SetTimeout(p, answer);
+}
 
 int main() {
 
-	const int kWindowWigth = 1280;
+    srand(unsigned int(time(nullptr)));
 
-	printf("%d\n", Min<int>(114, 514));
-	printf("%f\n", Min<float>(11.4f, 51.4f));
-	printf("%lf\n", Min<double>(11.4f, 51.4f));
-	printf("%c\n", Min<char>('v', 'x'));
+    void (*pFunc)(int a) = &DispResult;
 
-	return 0;
+    Game(pFunc, 3);
+
+    return 0;
 }
-
-
