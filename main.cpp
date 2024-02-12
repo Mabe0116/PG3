81dc7256455186445e2aa1b225d3e722d2e8db38
k022g0069@g.neec.ac.jp <k022g0069@g.neec.ac.jp>
2024-02-13 02:15:40 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -1,32 +1,69 @@
-#include <stdio.h>
-
-template <typename Type>
-
-Type Min(Type a, Type b) {
-	if (a < b) {
-		return b;
-	}
-	return a;
-}
-
-template <>
-char Min <char>(char a, char b)
-{
-	printf("数字以外は代入できません");
-	return 0;
-}
-
+#include <iostream>
+#include <list>
 
 int main() {
+	//駅のリスト
+	std::list<const char*> yamanote1970 = {
+		"Tokyo",
+		"Kanda",
+		"Akihabara",
+		"Okachumachi",
+		"Ueno",
+		"Uguisudani",
+		"Nippori",
+		"Tabata",
+		"Komagome",
+		"Sugamo",
+		"Otsuka",
+		"Ikebukuro",
+		"Mejiro",
+		"Takadanobaba",
+		"Shin-Okubo",
+		"Shinjuku",
+		"Yoyogi",
+		"Harajuku",
+		"Shibuya",
+		"Ebisu",
+		"Meguro",
+		"Gotanda",
+		"Osaki",
+		"Shinagawa",
+		"Tamachi",
+		"Hamamatsucho",
+		"Shimbashi",
+		"Yurakucho",
+	};
 
-	const int kWindowWigth = 1280;
 
-	printf("%d\n", Min<int>(114, 514));
-	printf("%f\n", Min<float>(11.4f, 51.4f));
-	printf("%lf\n", Min<double>(11.4f, 51.4f));
-	printf("%c\n", Min<char>('v', 'x'));
+	//1970年
+	std::cout << "Yamanote1970" << std::endl;
+	for (std::list<const char*>::iterator itr = yamanote1970.begin(); itr != yamanote1970.end(); ++itr) {
+		std::cout << *itr << std::endl;
+	}
+
+
+	//2001年
+	std::cout << "\nYamanote2001" << std::endl;
+	for (std::list<const char*>::iterator itr = yamanote1970.begin(); itr != yamanote1970.end(); ++itr) {
+		if (*itr == "Tabata") {
+			itr = yamanote1970.insert(itr, "Nishi-Nippori");
+			std::cout << *itr << std::endl;
+			itr++;
+		}
+		std::cout << *itr << std::endl;
+	}
+
+
+	//2022年
+	std::cout << "\nYamanote2022" << std::endl;
+	for (std::list<const char*>::iterator itr = yamanote1970.begin(); itr != yamanote1970.end(); ++itr) {
+		if (*itr == "Tamachi") {
+			itr = yamanote1970.insert(itr, "Takanawa-Gateway");
+			std::cout << *itr << std::endl;
+			++itr;
+		}
+		std::cout << *itr << std::endl;
+	}
 
 	return 0;
 }
-
-
